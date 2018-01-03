d6bd89678be8df5cb3be852c97234493844530d3
Joey Doyle <j.doyle345@gmail.com>
2018-01-02 18:44:27 -0800

    Started the lru project

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,92 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max) {false}
+  end
+
+  def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise ArgumentError("Out of bounds")
+    end
+  end
+
+  def remove(num)
+    if is_valid?(num)
+      @store[num] = false
+    end
+  end
+
+  def include?(num)
+    return true if @store[num] == true
+    false
+  end
+
+  private
+
+  def is_valid?(num)
+    return false if num >= @store.length
+    return false if num < 0
+    true
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
