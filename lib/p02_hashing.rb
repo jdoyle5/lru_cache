6bb4b9129a35ffbaa90c16712ec0fc997a232bbf
Joey Doyle <j.doyle345@gmail.com>
2018-01-23 12:00:35 -0800

    Finish the hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,15 +1,19 @@
 class Fixnum
   # Fixnum#hash already implemented for you
-  
+
 end
 
 class Array
   def hash
+    self.each_with_index.inject(0) do | accumulator, (el, i) |
+      (el.hash + i.hash) ^ accumulator
+    end
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -17,6 +21,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort_by(&:hash).hash
   end
 end
